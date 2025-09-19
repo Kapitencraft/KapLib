dd9eba8c0cbd1a5a5190a4bef906038004b98e80
Kapitencraft <kapitencraft@gmail.com>
2025-09-20 00:33:40 +0200

    update 1.27.7

--- src/main/java/net/kapitencraft/kap_lib/enchantments/abstracts/EnchantmentBowEffect.java
+++ src/main/java/net/kapitencraft/kap_lib/enchantments/abstracts/EnchantmentBowEffect.java
@@ -4,21 +4,27 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
+import net.kapitencraft.kap_lib.registry.ExtraEnchantmentEffectComponents;
 import net.kapitencraft.kap_lib.registry.custom.core.ExtraRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.ConditionalEffect;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import org.apache.commons.lang3.mutable.MutableFloat;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -31,34 +37,35 @@ public interface EnchantmentBowEffect {
             .required(LootContextParams.ENCHANTMENT_LEVEL)
             .optional(LootContextParams.ATTACKING_ENTITY).build();
 
-    static int getLevel(CompoundTag tag) {
-        return tag.getInt("Level");
-    }
-
     @ApiStatus.Internal
     static float loadFromTag(LivingEntity target, CompoundTag tag, ExePhase type, float oldDamage, AbstractArrow arrow) {
         if (arrow.level() instanceof ServerLevel serverLevel) {
             LootParams.Builder builder = new LootParams.Builder(serverLevel);
             builder.withParameter(LootContextParams.THIS_ENTITY, arrow)
                     .withOptionalParameter(LootContextParams.ATTACKING_ENTITY, target);
-            for (ResourceLocation location : executionMap.keySet()) {
-                String string = location.toString();
-                if (tag.contains(string, 10)) {
-                    CompoundTag elementTag = tag.getCompound(string);
-                    int level = getLevel(elementTag);
-                    builder.withParameter(LootContextParams.ENCHANTMENT_LEVEL, level);
-                    for (ConditionalEffect<EnchantmentBowEffect> execution : executionMap.get(location)) {
-                        if (execution.matches(new LootContext.Builder(builder.create(EnchantmentBowEffect.LOOT_PARAM_SET)).create(Optional.empty())))
-                            oldDamage = execution.effect().execute(level, target, elementTag, type, oldDamage, arrow);
+            ItemStack weaponItem = arrow.getWeaponItem();
+            if (!weaponItem.isEmpty()) {
+                MutableFloat damage = new MutableFloat(oldDamage);
+
+                EnchantmentHelper.runIterationOnItem(weaponItem, (holder, enchLevel) -> {
+                    builder.withParameter(LootContextParams.ENCHANTMENT_LEVEL, enchLevel);
+                    List<ConditionalEffect<EnchantmentBowEffect>> effects = holder.value().getEffects(ExtraEnchantmentEffectComponents.BOW.value());
+                    ListTag list = tag.getList(holder.getKey().location().toString(), CompoundTag.TAG_COMPOUND);
+                    for (int i = 0; i < effects.size(); i++) {
+                        ConditionalEffect<EnchantmentBowEffect> effect = effects.get(i);
+                        if (effect.matches(new LootContext.Builder(builder.create(EnchantmentBowEffect.LOOT_PARAM_SET)).create(Optional.empty())) && (type != ExePhase.TICK || effect.effect().shouldTick())) {
+                            effect.effect().execute(enchLevel, target, list.getCompound(i), type, damage, arrow);
+                        }
                     }
-                }
+                });
+                return damage.floatValue();
             }
         }
         return oldDamage;
     }
 
     /**
-     * use to add extra tags which are needed in {@link EnchantmentBowEffect#execute(int, LivingEntity, CompoundTag, ExePhase, float, AbstractArrow) execute}, to the bow
+     * use to add extra tags which are needed in {@link EnchantmentBowEffect#execute(int, LivingEntity, CompoundTag, ExePhase, MutableFloat, AbstractArrow) execute}, to the bow
      * the enchantment level is written automatically
      * @return the populated data
      */
@@ -69,11 +76,10 @@ public interface EnchantmentBowEffect {
      * @param target the hit entity, or null if it hit a block, or it's a tick event (see {@code type})
      * @param tag the data saved to the arrow via the {@link EnchantmentBowEffect#write(CompoundTag, int, ItemStack, LivingEntity, AbstractArrow) write} method
      * @param type the type of the execution. either TICK or HIT
-     * @param oldDamage the damage the arrow would do (only HIT)
+     * @param damage the damage the arrow would do (only HIT)
      * @param arrow the arrow that's currently used
-     * @return the new damage value (ignored in TICK phase)
      */
-    float execute(int level, @Nullable LivingEntity target, CompoundTag tag, ExePhase type, float oldDamage, AbstractArrow arrow);
+    void execute(int level, @Nullable LivingEntity target, CompoundTag tag, ExePhase type, MutableFloat damage, AbstractArrow arrow);
 
     /**
      * determines whether the enchantment should tick on arrows
--- src/main/java/net/kapitencraft/kap_lib/registry/ExtraEnchantmentEffectComponents.java
+++ src/main/java/net/kapitencraft/kap_lib/registry/ExtraEnchantmentEffectComponents.java
@@ -29,7 +29,7 @@ public interface ExtraEnchantmentEffectComponents {
     DeferredHolder<DataComponentType<?>, DataComponentType<List<ConditionalEffect<EnchantmentEntityEffect>>>> BOW_SPAWN = register("projectile_spawn", builder ->
             builder.persistent(ConditionalEffect.codec(EnchantmentEntityEffect.CODEC, LootContextParamSets.ENCHANTED_ENTITY).listOf()));
     DeferredHolder<DataComponentType<?>, DataComponentType<List<ConditionalEffect<EnchantmentBowEffect>>>> BOW = register("bow_tick", builder ->
-            builder.persistent(ConditionalEffect.codec(EnchantmentBowEffect.CODEC, LootContextParamSets.ENCHANTED_ENTITY).listOf()));
+            builder.persistent(ConditionalEffect.codec(EnchantmentBowEffect.CODEC, EnchantmentBowEffect.LOOT_PARAM_SET).listOf()));
     DeferredHolder<DataComponentType<?>, DataComponentType<List<TargetedConditionalEffect<EnchantmentCountEffect>>>> COUNT = register("count", builder ->
             builder.persistent(TargetedConditionalEffect.codec(EnchantmentCountEffect.CODEC, LootContextParamSets.ENCHANTED_DAMAGE).listOf()));
     DeferredHolder<DataComponentType<?>, DataComponentType<List<ConditionalEffect<EnchantmentBlockBreakEffect>>>> BLOCK_BREAK = register("block_break", listBuilder ->
