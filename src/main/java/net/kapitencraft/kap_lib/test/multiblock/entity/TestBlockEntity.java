d366e158c3a0ddff23e53adcffb4ff4f3d416b3f
Kapitencraft <kapitencraft@gmail.com>
2026-01-24 23:53:07 +0100

    BEs appear to sync

---
+++ src/main/java/net/kapitencraft/kap_lib/test/multiblock/entity/TestBlockEntity.java
@@ -0,0 +1,47 @@
+package net.kapitencraft.kap_lib.test.multiblock.entity;
+
+import net.kapitencraft.kap_lib.test.registry.TestBlockEntityTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class TestBlockEntity extends BlockEntity implements MenuProvider {
+    private final SimpleContainer inventory = new SimpleContainer(54);
+
+    public TestBlockEntity(BlockPos pos, BlockState blockState) {
+        super(TestBlockEntityTypes.TEST.get(), pos, blockState);
+    }
+
+    @Override
+    public @NotNull Component getDisplayName() {
+        return Component.translatable("container.test");
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+        tag.put("inventory", inventory.createTag(registries));
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+        this.inventory.fromTag(tag.getList("inventory", 10), registries);
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int containerId, Inventory playerInventory, Player player) {
+        return ChestMenu.sixRows(containerId, playerInventory, inventory);
+    }
+}
