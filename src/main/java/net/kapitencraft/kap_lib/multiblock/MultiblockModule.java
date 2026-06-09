48f315ebe5916d13920101b68d0a9118c3627f6d
Kapitencraft <kapitencraft@gmail.com>
2026-06-09 23:55:00 +0200

    work on making the configuration screen work + added item for configuration block

--- src/main/java/net/kapitencraft/kap_lib/multiblock/MultiblockModule.java
+++ src/main/java/net/kapitencraft/kap_lib/multiblock/MultiblockModule.java
@@ -1,8 +1,9 @@
 package net.kapitencraft.kap_lib.multiblock;
 
 import net.kapitencraft.kap_lib.core.LibConstants;
-import net.kapitencraft.kap_lib.multiblock.registry.ModBlockEntityTypes;
-import net.kapitencraft.kap_lib.multiblock.registry.ModBlocks;
+import net.kapitencraft.kap_lib.multiblock.registry.MBBlockEntityTypes;
+import net.kapitencraft.kap_lib.multiblock.registry.MBBlocks;
+import net.kapitencraft.kap_lib.multiblock.registry.MBItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -12,7 +13,8 @@ public class MultiblockModule {
     public static final String MODULE_ID = LibConstants.MOD_ID + "_multiblock";
 
     public MultiblockModule(IEventBus modEventBus, ModContainer container) {
-        ModBlockEntityTypes.REGISTRY.register(modEventBus);
-        ModBlocks.REGISTRY.register(modEventBus);
+        MBBlockEntityTypes.REGISTRY.register(modEventBus);
+        MBBlocks.REGISTRY.register(modEventBus);
+        MBItems.REGISTRY.register(modEventBus);
     }
 }
---
+++ src/main/java/net/kapitencraft/kap_lib/multiblock/registry/MBItems.java
@@ -0,0 +1,8 @@
+package net.kapitencraft.kap_lib.multiblock.registry;
+
+import net.kapitencraft.kap_lib.core.LibConstants;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public interface MBItems {
+    DeferredRegister.Items REGISTRY = DeferredRegister.createItems(LibConstants.MOD_ID);
+}
