e17a6530098ec8f4982aa1690990f9897fff9301
Kapitencraft <kapitencraft@gmail.com>
2026-06-09 23:55:00 +0200

    work on making the configuration screen work + added item for configuration block

--- src/main/java/net/kapitencraft/kap_lib/multiblock/structure/config/MultiblockStructureConfigurationEditScreen.java
+++ src/main/java/net/kapitencraft/kap_lib/multiblock/structure/config/MultiblockStructureConfigurationEditScreen.java
@@ -1,17 +1,28 @@
 package net.kapitencraft.kap_lib.multiblock.structure.config;
 
-import net.kapitencraft.kap_lib.multiblock.registry.ModBlocks;
+import com.google.common.collect.ImmutableList;
+import net.kapitencraft.kap_lib.multiblock.registry.MBBlocks;
+import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.StructureBlockEditScreen;
 import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
 
 public class MultiblockStructureConfigurationEditScreen extends Screen {
+    private static final ImmutableList<MultiblockStructureConfigurationBlockEntity.Mode> ALL_MODES = ImmutableList.copyOf(MultiblockStructureConfigurationBlockEntity.Mode.values());
+    private static final ImmutableList<MultiblockStructureConfigurationBlockEntity.Mode> DEFAULT_MODES = ALL_MODES.stream()
+            .filter(p_169859_ -> p_169859_ != MultiblockStructureConfigurationBlockEntity.Mode.SAVE)
+            .collect(ImmutableList.toImmutableList());
+
+    private MultiblockStructureConfigurationBlockEntity.Mode initialMode = MultiblockStructureConfigurationBlockEntity.Mode.SAVE;
+
     private final MultiblockStructureConfigurationBlockEntity configuration;
     private EditBox sizeXEdit, sizeYEdit, sizeZEdit;
+    private EditBox nameEdit;
     
     protected MultiblockStructureConfigurationEditScreen(MultiblockStructureConfigurationBlockEntity configuration) {
-        super(Component.translatable(ModBlocks.MULTIBLOCK_STRUCTURE_CONFIG.get().getDescriptionId()));
+        super(Component.translatable(MBBlocks.MULTIBLOCK_STRUCTURE_CONFIG.get().getDescriptionId()));
         this.configuration = configuration;
     }
 
@@ -31,5 +42,42 @@ public class MultiblockStructureConfigurationEditScreen extends Screen {
         this.sizeZEdit.setMaxLength(15);
         this.sizeZEdit.setValue(Integer.toString(vec3i.getZ()));
         this.addWidget(this.sizeZEdit);
+        this.nameEdit = new EditBox(this.font, this.width / 2 - 152, 40, 300, 20, Component.translatable("structure_block.structure_name")) {
+            @Override
+            public boolean charTyped(char p_99476_, int p_99477_) {
+                return MultiblockStructureConfigurationEditScreen.this.isValidCharacterForName(this.getValue(), p_99476_, this.getCursorPosition()) && super.charTyped(p_99476_, p_99477_);
+            }
+        };
+        this.nameEdit.setMaxLength(128);
+        this.nameEdit.setValue(this.configuration.getStructureName());
+        this.addWidget(this.nameEdit);
+
+        this.addRenderableWidget(
+                CycleButton.<MultiblockStructureConfigurationBlockEntity.Mode>builder(p_169852_ -> Component.translatable("structure_block.mode." + p_169852_.getSerializedName()))
+                        .withValues(DEFAULT_MODES, ALL_MODES)
+                        .displayOnlyValue()
+                        .withInitialValue(this.initialMode)
+                        .create(this.width / 2 - 4 - 150, 185, 50, 20, Component.literal("MODE"), (p_169846_, mode) -> {
+                            this.configuration.setMode(mode);
+                            this.updateMode(mode);
+                        })
+        );
+    }
+
+    private void updateMode(MultiblockStructureConfigurationBlockEntity.Mode structureMode) {
+        this.nameEdit.setVisible(false);
+        this.sizeXEdit.setVisible(false);
+        this.sizeYEdit.setVisible(false);
+        this.sizeZEdit.setVisible(false);
+        switch (structureMode) {
+            case SAVE:
+                this.sizeXEdit.setVisible(true);
+                this.sizeYEdit.setVisible(true);
+                this.sizeZEdit.setVisible(true);
+                break;
+            case CORNER:
+                this.nameEdit.setVisible(true);
+                break;
+        }
     }
 }
