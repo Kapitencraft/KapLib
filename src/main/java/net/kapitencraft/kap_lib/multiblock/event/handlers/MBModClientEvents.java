a79ccf348ab0746e8fea65278f479770e3f33815
Kapitencraft <kapitencraft@gmail.com>
2026-06-10 23:52:10 +0200

    added BER to multiblock structure config block

---
+++ src/main/java/net/kapitencraft/kap_lib/multiblock/event/handlers/MBModClientEvents.java
@@ -0,0 +1,16 @@
+package net.kapitencraft.kap_lib.multiblock.event.handlers;
+
+import net.kapitencraft.kap_lib.multiblock.registry.MBBlockEntityTypes;
+import net.kapitencraft.kap_lib.multiblock.structure.config.MultiblockStructureConfigurationBlockEntityRenderer;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+
+@EventBusSubscriber
+public class MBModClientEvents {
+
+    @SubscribeEvent
+    public static void onEntityRenderersRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(MBBlockEntityTypes.MULTIBLOCK_STRUCTURE_CONFIG.get(), MultiblockStructureConfigurationBlockEntityRenderer::new);
+    }
+}
