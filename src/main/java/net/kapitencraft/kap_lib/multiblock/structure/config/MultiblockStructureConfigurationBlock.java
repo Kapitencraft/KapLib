7138afbab0554ebf02efbbde609bb3ee97a22ac3
Kapitencraft <kapitencraft@gmail.com>
2026-06-10 23:52:10 +0200

    added BER to multiblock structure config block

--- src/main/java/net/kapitencraft/kap_lib/multiblock/structure/config/MultiblockStructureConfigurationBlock.java
+++ src/main/java/net/kapitencraft/kap_lib/multiblock/structure/config/MultiblockStructureConfigurationBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.StructureBlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -36,5 +37,10 @@ public class MultiblockStructureConfigurationBlock extends BaseEntityBlock {
         } else {
             return InteractionResult.PASS;
         }
+    }
+
+    @Override
+    protected RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 }
