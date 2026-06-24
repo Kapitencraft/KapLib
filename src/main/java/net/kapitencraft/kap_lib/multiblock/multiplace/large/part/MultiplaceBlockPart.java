914f5ec4aa22464d4f9fd9eeda3dd89d0283c638
Kapitencraft <kapitencraft@gmail.com>
2026-06-24 15:17:45 +0200

    moved multiblock package where it belonged

--- src/main/java/net/kapitencraft/kap_lib/multiblock/multiplace/large/part/MultiplaceBlockPart.java
+++
@@ -1,9 +0,0 @@
-package net.kapitencraft.kap_lib.multiblock.multiplace.large.part;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.StringRepresentable;
-
-public interface MultiplaceBlockPart<P extends MultiplaceBlockPart<P>> extends Comparable<P>, StringRepresentable {
-
-    BlockPos getOffset();
-}
