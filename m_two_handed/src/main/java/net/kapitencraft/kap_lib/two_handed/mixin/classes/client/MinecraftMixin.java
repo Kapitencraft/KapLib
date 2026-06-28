d25d0987d9a074b3cb7b8a69022a0f7c90e48351
Kapitencraft <kapitencraft@gmail.com>
2026-06-28 02:13:28 +0200

    error: attribute swap does not appear to be working correctly

--- m_two_handed/src/main/java/net/kapitencraft/kap_lib/two_handed/mixin/classes/client/MinecraftMixin.java
+++ m_two_handed/src/main/java/net/kapitencraft/kap_lib/two_handed/mixin/classes/client/MinecraftMixin.java
@@ -2,6 +2,7 @@ package net.kapitencraft.kap_lib.two_handed.mixin.classes.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
@@ -10,14 +11,15 @@ import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import javax.annotation.Nullable;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
-    @Shadow @Nullable public HitResult hitResult;
+    @Shadow
+    @Nullable
+    public HitResult hitResult;
 
     @WrapOperation(method = "startAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"))
     private ItemStack includeOffhandAttack(LocalPlayer instance, InteractionHand hand, Operation<ItemStack> original) {
@@ -26,5 +28,11 @@ public class MinecraftMixin {
         return original.call(instance, hand);
     }
 
-    @ModifyConstant
+    @WrapOperation(method = "startAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;swing(Lnet/minecraft/world/InteractionHand;)V"))
+    private void includeOffhandSwing(LocalPlayer instance, InteractionHand hand, Operation<Void> original, @Local ItemStack itemstack) {
+        if (itemstack == instance.getMainHandItem())
+            original.call(instance, hand);
+        else
+            original.call(instance, InteractionHand.OFF_HAND);
+    }
 }
