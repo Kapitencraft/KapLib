28332e9269b4f639e712e23f2b5134393c396315
Kapitencraft <kapitencraft@gmail.com>
2026-07-11 02:36:21 +0200

    added line-multiplace block entities

--- m_multiblock/src/main/java/net/kapitencraft/kap_lib/multiblock/test/multiblock/Mp3x1TestBlock.java
+++ m_multiblock/src/main/java/net/kapitencraft/kap_lib/multiblock/test/multiblock/Mp3x1TestBlock.java
@@ -1,10 +1,20 @@
 package net.kapitencraft.kap_lib.multiblock.test.multiblock;
 
 import net.kapitencraft.kap_lib.multiblock.multiplace.line.Multiplace3x1Block;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 public class Mp3x1TestBlock extends Multiplace3x1Block {
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+
     public Mp3x1TestBlock(Properties properties) {
         super(properties);
     }
@@ -12,5 +22,32 @@ public class Mp3x1TestBlock extends Multiplace3x1Block {
     @Override
     protected DirectionProperty getDirectionProperty() {
         return BlockStateProperties.FACING;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(LIT);
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
+        if (!level.isClientSide) {
+            boolean flag = state.getValue(LIT);
+            if (flag != level.hasNeighborSignal(pos)) {
+                if (flag) {
+                    level.scheduleTick(pos, this, 4);
+                } else {
+                    setState(level, pos, state.cycle(LIT), 2);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(LIT) && !level.hasNeighborSignal(pos)) {
+            setState(level, pos, state.cycle(LIT), 2);
+        }
     }
 }
--- m_multiblock/src/main/java/net/kapitencraft/kap_lib/multiblock/test/registry/TestBlocks.java
+++ m_multiblock/src/main/java/net/kapitencraft/kap_lib/multiblock/test/registry/TestBlocks.java
@@ -22,7 +22,7 @@ public interface TestBlocks {
     DeferredBlock<Mp2x2BlockTest> MP_2X2_TEST = registerBlock("mp_2x2_test", Mp2x2BlockTest::new);
     DeferredBlock<Mp2x3BlockTest> MP_2X3_TEST = registerBlock("mp_2x3_test", Mp2x3BlockTest::new);
     DeferredBlock<Mp3x3BlockTest> MP_3X3_TEST = registerBlock("mp_3x3_test", Mp3x3BlockTest::new);
-    DeferredBlock<Mp2CubedBlockTest> MP_2_CUBED_TEST = registerBlock("mp_2_cubed_test", Mp2CubedBlockTest::new);
+    DeferredBlock<Mp2CubedEntityBlockTest> MP_2_CUBED_TEST = registerBlock("mp_2_cubed_test", Mp2CubedEntityBlockTest::new);
 
     private static <T extends Block, K extends BlockItem> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> block) {
         DeferredBlock<T> toReturn = REGISTRY.registerBlock(name, block);
