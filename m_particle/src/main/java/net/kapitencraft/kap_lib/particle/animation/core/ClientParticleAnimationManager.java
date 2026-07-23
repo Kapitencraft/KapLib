946aa5587873a94a1791f3111b04279c2c9ef762
Kapitencraft <kapitencraft@gmail.com>
2026-07-23 10:52:22 +0200

    updated particle animation managers to use CodecJsonReloaders instead of simple json reloaders

--- m_particle/src/main/java/net/kapitencraft/kap_lib/particle/animation/core/ClientParticleAnimationManager.java
+++ m_particle/src/main/java/net/kapitencraft/kap_lib/particle/animation/core/ClientParticleAnimationManager.java
@@ -5,6 +5,7 @@ import com.mojang.logging.LogUtils;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
 import net.kapitencraft.kap_lib.core.io.JsonHelper;
+import net.kapitencraft.kap_lib.core.io.serialization.CodecJsonReloader;
 import net.kapitencraft.kap_lib.particle.animation.activation_triggers.core.ActivationTrigger;
 import net.kapitencraft.kap_lib.particle.animation.activation_triggers.core.ActivationTriggerInstance;
 import net.kapitencraft.kap_lib.particle.animation.store.ParticleAnimationPreset;
@@ -30,7 +31,7 @@ import java.util.Map;
  * manager of all animations
  */
 //TODO store animations in JSON and load them via reference
-public final class ClientParticleAnimationManager extends SimpleJsonResourceReloadListener {
+public final class ClientParticleAnimationManager extends CodecJsonReloader<ParticleAnimationPreset> {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public static final ClientParticleAnimationManager INSTANCE = new ClientParticleAnimationManager();
@@ -47,7 +48,7 @@ public final class ClientParticleAnimationManager extends SimpleJsonResourceRelo
     private final Map<ParticleAnimator, List<ActivationTriggerInstance>> onHold = new HashMap<>();
 
     public ClientParticleAnimationManager() {
-        super(JsonHelper.GSON, "animation_presets");
+        super(ParticleAnimationPreset.CODEC, JsonHelper.GSON, "animation_presets");
     }
 
     public static void activate(List<ParticleAnimation> animations) {
@@ -127,12 +128,8 @@ public final class ClientParticleAnimationManager extends SimpleJsonResourceRelo
     }
 
     @Override
-    protected void apply(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler) {
+    protected void apply(Map<ResourceLocation, ParticleAnimationPreset> presetMap, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
         this.presets.clear();
-        object.forEach((location, jsonElement) -> {
-            DataResult<ParticleAnimationPreset> result = ParticleAnimationPreset.CODEC.parse(JsonOps.INSTANCE, jsonElement);
-            result.resultOrPartial(s -> LOGGER.warn("error parsing particle animation preset {}: {}", location, s))
-                    .ifPresent(p -> this.presets.put(location, p));
-        });
+        this.presets.putAll(presetMap);
     }
 }
