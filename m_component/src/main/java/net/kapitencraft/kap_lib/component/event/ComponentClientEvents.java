4ccb541702d30e2202481ac293951e9fc0f696c4
Kapitencraft <kapitencraft@gmail.com>
2026-07-25 10:39:06 +0200

    import fixes and rename

--- m_component/src/main/java/net/kapitencraft/kap_lib/component/event/ComponentClientEvents.java
+++ m_component/src/main/java/net/kapitencraft/kap_lib/component/event/ComponentClientEvents.java
@@ -1,13 +1,13 @@
 package net.kapitencraft.kap_lib.component.event;
 
 import net.kapitencraft.kap_lib.component.player_head.PlayerHeadAllocator;
-import net.kapitencraft.kap_lib.core.event.custom.FontSetsEvent;
+import net.kapitencraft.kap_lib.core.event.custom.client.FontSetsEvent;
 import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber
-public class ModClientEvents {
+public class ComponentClientEvents {
 
     @SubscribeEvent
     public static void onFontSetsRegister(FontSetsEvent.Register event) {
--- m_enchantment/src/main/java/net/kapitencraft/kap_lib/enchantment/event/handler/EnchantmentClientEvents.java
+++ m_enchantment/src/main/java/net/kapitencraft/kap_lib/enchantment/event/handler/EnchantmentClientEvents.java
@@ -1,12 +1,12 @@
 package net.kapitencraft.kap_lib.enchantment.event.handler;
 
-import net.kapitencraft.kap_lib.core.event.custom.FontSetsEvent;
+import net.kapitencraft.kap_lib.core.event.custom.client.FontSetsEvent;
 import net.kapitencraft.kap_lib.enchantment.client.enchantment_applicable.EnchantmentApplicableAllocator;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber
-public class ModClientEvents {
+public class EnchantmentClientEvents {
 
     @SubscribeEvent
     public static void onFontSetsRegister(FontSetsEvent.Register event) {
